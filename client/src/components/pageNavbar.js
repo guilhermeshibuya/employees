ab3c5881af54a84a1c23ca0c4b1476a83ff27459
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2023-03-05 20:32:02 -0300

    Navbar

--- client/src/components/pageNavbar.js
+++ client/src/components/pageNavbar.js
@@ -8,12 +8,12 @@ const PageNavbar = () => {
     return (
         <Fragment>
             <Navbar bg="light" expand="lg">
-                <Container className="mx-0">
+                <Container fluid className="mx-0">
                     <Navbar.Brand as={NavLink} to="/">CRUD</Navbar.Brand>
 
                     <Navbar.Toggle aria-controls="navbar-nav"/>
                     <Navbar.Collapse id="navbar-nav">
-                        <Nav>
+                        <Nav className="ms-auto">
                             <Nav.Link as={NavLink} to="/create">Create Record</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
